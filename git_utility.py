83553187d30686f684fd9d0fbb496bf8c942122a
Sourabh Prasanna Kumar <sprasanna@planview.com>
2024-07-01 19:04:12 +0530

    Added get_branches method to list all branches

--- git_utility.py
+++ git_utility.py
@@ -8,6 +8,12 @@ import os
 def cli():
     pass
 
+@click.command()
+@click.option("-d", "--directory", default=".", type=str, help="Target path get git status of")
+def get_branches(directory):
+    os.chdir(directory)
+    result = subprocess.run(["git", "branch"], capture_output=True, text=True)
+    print(result.stdout)
 
 @click.command()
 @click.option("-d", "--directory", default=".", type=str, help="Target path get git status of")
@@ -18,6 +24,8 @@ def get_status(directory):
 
 
 cli.add_command(get_status)
+cli.add_command(get_branches)
+
 
 if __name__ == '__main__':
     cli()
